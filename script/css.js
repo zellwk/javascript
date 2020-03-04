be1dc359f42134407ecea91a7d02b7ab9d265ba4
Zell Liew <zellwk@gmail.com>
2020-03-04 12:07:25 +0800

    Improve gettranslateValues

--- script/css.js
+++ script/css.js
@@ -5,21 +5,33 @@
  * Gets computed translate values
  * @param {HTMLElement} element
  * @returns {Object}
+ * @see https://zellwk.com/blog/translate-in-javascript
  */
 export function getTranslateValues (element) {
   const style = window.getComputedStyle(element)
   const matrix = style.transform || style.webkitTransform || style.mozTransform
+
+  // No transform property. Simply return 0 values.
+  if (matrix === 'none') {
+    return {
+      x: 0,
+      y: 0,
+      z: 0
+    }
+  }
 
   // Can either be 2d or 3d transform
   const matrixType = matrix.includes('3d') ? '3d' : '2d'
   const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ')
 
   // 2d Matrixes have 6 values
-  // Last 2 values are X and Y
+  // Last 2 values are X and Y.
+  // 2d Matrixes does not have Z value.
   if (matrixType === '2d') {
     return {
       x: matrixValues[4],
-      y: matrixValues[5]
+      y: matrixValues[5],
+      z: 0
     }
   }
 
