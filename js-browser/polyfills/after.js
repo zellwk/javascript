ced51a103cf494460dc21bdb09138cdaca5f47dc
Zell Liew <zellwk@gmail.com>
2020-04-21 22:11:46 +0800

    Add before after polyfill

---
+++ js-browser/polyfills/after.js
@@ -0,0 +1,34 @@
+/* eslint-env browser */
+
+/**
+ * Element.before polyfill
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/after
+ */
+export function afterPolyfill () {
+  (function (arr) {
+    arr.forEach(function (item) {
+      /* eslint-disable */
+      if (item.hasOwnProperty('after')) {
+        return
+      }
+      /* eslint-enable */
+
+      Object.defineProperty(item, 'after', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function after () {
+          var argArr = Array.prototype.slice.call(arguments)
+          var docFrag = document.createDocumentFragment()
+
+          argArr.forEach(function (argItem) {
+            var isNode = argItem instanceof Node
+            docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)))
+          })
+
+          this.parentNode.insertBefore(docFrag, this.nextSibling)
+        }
+      })
+    })
+  })([Element.prototype, CharacterData.prototype, DocumentType.prototype])
+}
