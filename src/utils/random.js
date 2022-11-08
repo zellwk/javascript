3411ce5dad18a594a7f0767267cde99217ee319e
Zell Liew <zellwk@gmail.com>
2022-11-08 12:31:35 +0800

    cleanup

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,12 +1,12 @@
-import EnvCtx from './env.js'
-const envCtx = EnvCtx()
+import getEnv from './env.js'
+const env = getEnv()
 
-export async function randomString () {
-  if (envCtx === 'browser') {
+export async function randomString() {
+  if (env === 'browser') {
     return crypto.getRandomValues(new Uint32Array(5)).join('')
   }
 
-  if (envCtx === 'node') {
+  if (env === 'node') {
     const crypto = await import('crypto')
     return crypto.randomBytes(32).toString('base64')
   }
