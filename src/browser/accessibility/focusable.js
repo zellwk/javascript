2516ae8d24e22e782a9af885d05f1fa7677e51b7
Zell Liew <zellwk@gmail.com>
2023-03-25 22:16:17 +0800

    Remove completed task

--- src/browser/accessibility/focusable.js
+++ src/browser/accessibility/focusable.js
@@ -1,16 +1,29 @@
-// TODO: Make focusable elements list more complete.
-//  - See #2 and #7 for info.
-export default function Focusable (element = document) {
+export default function Focusable(element = document) {
   return {
-    get length () {
+    get length() {
       return this.keyboardOnly.length
     },
 
-    get all () {
+    get all() {
       return [
         ...element.querySelectorAll(
-          'a, button, input, textarea, select, details, iframe, embed, object, summary dialog, audio[controls], video[controls], [contenteditable], [tabindex]'
-        )
+          `a, 
+            button, 
+            input, 
+            textarea, 
+            select, 
+            details, 
+            iframe, 
+            embed, 
+            object, 
+            summary, 
+            dialog, 
+            audio[controls], 
+            video[controls], 
+            [contenteditable], 
+            [tabindex]
+          `
+        ),
       ].filter(el => {
         return (
           !el.hasAttribute('disabled') &&
@@ -20,16 +33,16 @@ export default function Focusable (element = document) {
       })
     },
 
-    get keyboardOnly () {
+    get keyboardOnly() {
       return this.all.filter(el => el.tabIndex > -1)
     },
 
-    get firstFocusable () {
+    get firstFocusable() {
       return this.keyboardOnly[0]
     },
 
-    get lastFocusable () {
+    get lastFocusable() {
       return this.keyboardOnly[this.length - 1]
-    }
+    },
   }
 }
