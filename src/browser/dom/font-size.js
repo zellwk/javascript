62b9d9b0c6b8f40cdf3325f74f81b78c1ee271db
Zell Liew <zellwk@gmail.com>
2023-03-25 14:29:46 +0800

    Make em and rem more functional

--- src/browser/dom/font-size.js
+++ src/browser/dom/font-size.js
@@ -1,10 +1,10 @@
 /* globals getComputedStyle */
-export function em (element, multiple) {
+export function em(element = document.body, multiple = 1) {
   const unit = parseInt(getComputedStyle(element)['font-size'])
   return unit * multiple
 }
 
-export function rem (multiple) {
+export function rem(multiple = 1) {
   const unit = parseInt(getComputedStyle(document.body)['font-size'])
   return unit * multiple
 }
