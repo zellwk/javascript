d243fb0d7e9f2f373b9de383e6b4bbcf5a56fd45
Zell Liew <zellwk@gmail.com>
2023-03-28 09:12:18 +0800

    Add padding values to boundingBox

--- src/browser/dom/elem.js
+++ src/browser/dom/elem.js
@@ -1,45 +1,65 @@
+/**
+ * Gets siblings of the specified element
+ * @param {HTMLElement} element
+ */
+export function siblings(element) {
+  return [...element.parentElement.children].filter(el => el !== element)
+}
+
+/**
+ * Gets the Bounding box with information about
+ * horizontal and vertical centers of the box
+ * @param {HTMLElement} element
+ */
+export function boundingBox(element) {
+  const box = element.getBoundingClientRect()
+  const style = window.getComputedStyle(element)
+
+  const ret = {}
+  const padding = {
+    top: parseFloat(style.paddingTop),
+    right: parseFloat(style.paddingRight),
+    bottom: parseFloat(style.paddingBottom),
+    left: parseFloat(style.paddingLeft),
+  }
+
+  for (const prop in box) {
+    ret[prop] = box[prop]
+  }
+
+  ret.xCenter = (box.left + box.right) / 2
+  ret.yCenter = (box.top + box.bottom) / 2
+  ret.padding = padding
+
+  // Return accurate width and height measurements for use with animations
+  const boxSizing = style.boxSizing
+  if (boxSizing === 'content-box') {
+    ret.width = box.width - padding.left - padding.right
+    ret.height = box.height - padding.top - padding.bottom
+  } else if (boxSizing === 'border-box') {
+    ret.contentWidth = box.width - padding.left - padding.right
+    ret.contentHeight = box.height - padding.top - padding.bottom
+  }
+
+  return ret
+}
+
 export default {
   /**
    * Gets siblings of the specified element
+   * @deprecated
    * @param {HTMLElement} element
    */
   siblings(element) {
-    return [...element.parentElement.children].filter(el => el !== element)
+    console.warn('Default export is deprecated. Use named export instead.')
+    siblings(element)
   },
 
   /**
    * Gets the Bounding box with information about
    * horizontal and vertical centers of the box
+   * @deprecated
    * @param {HTMLElement} element
    */
-  boundingBox(element) {
-    const box = element.getBoundingClientRect()
-    const style = window.getComputedStyle(box)
-
-    const ret = {}
-
-    for (const prop in box) {
-      ret[prop] = box[prop]
-    }
-
-    ret.xCenter = (box.left + box.right) / 2
-    ret.yCenter = (box.top + box.bottom) / 2
-
-    // Adjust Width and Height if Box Sizing is content-box, which shouldn't be the case for most sites... but just in case when working with other people's code.
-    const boxSizing = style.boxSizing
-
-    if (boxSizing === 'content-box') {
-      const padding = {
-        top: parseInt(style.paddingTop),
-        right: parseInt(style.paddingRight),
-        bottom: parseInt(style.paddingBottom),
-        left: parseInt(style.paddingLeft),
-      }
-
-      ret.width = box.width - padding.left - padding.right
-      ret.height = box.height - padding.top - padding.bottom
-    }
-
-    return ret
-  },
+  boundingBox(element) {},
 }
